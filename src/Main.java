edd3b49d524c32f960ab2426a1c4c5b7392ac9f6
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-08 18:43:56 -0700

    Updated file structure

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,4 @@
-import memento.exercise.Document;
-import memento.exercise.DocumentHistory;
-import memento.exercise.DocumentState;
-
 public class Main {
     public static void main(String[] args) {
-        var document = new Document();
-        var documentHistory = new DocumentHistory();
-
-        document.setContent("content");
-        document.setFontName("Arial");
-        document.setFontSize("20");
-
-        documentHistory.add(document.createState());
-
-        document.setContent("content");
-        document.setFontName("Bread");
-        document.setFontSize("30");
-
-        documentHistory.add(document.createState());
-        document.restoreState(documentHistory.pop());
-
-        System.out.println(document.getContent() + " " +
-                document.getFontName() + " " +
-                document.getFontSize());
     }
 }
