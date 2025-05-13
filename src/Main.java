202d56b8c9adc18783deb0417eb87f1b87cd09c2
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-13 13:29:51 -0700

    Partially reviewed chain of responsibility

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import mediator.ArticlesDialogBox;
-
-import java.awt.*;
+import chainOfResponsibility.*;
 
 public class Main {
     public static void main(String[] args) {
-        var dialog = new ArticlesDialogBox();
-        dialog.simulateUserInteraction();
+        var compressor = new Compressor(null);
+        var logger = new Logger(compressor);
+        var authenticator = new Authenticator(logger);
+
+        var server = new WebServer(authenticator);
+        server.handle(new HttpRequest("admin", ""));
     }
 }
