f35a58f6611be13499750dadff0b99659751abc6
Alejandro Navarro <alexnavarro57@gmail.com>
2025-05-12 19:50:07 -0700

    Partially reviwed builder pattern

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 import abstractFactory.exercise.Demo;
+import builder.MovieBuilder;
+import builder.PdfDocumentBuilder;
+import builder.Presentation;
+import builder.Slide;
 
 public class Main {
     public static void main(String[] args) {
-        Demo.show();
+        var presentation = new Presentation();
+        presentation.addSlide(new Slide("Slide 1"));
+        presentation.addSlide(new Slide("Slide 2"));
+
+        var builder = new MovieBuilder();
+        presentation.export(builder);
+        var movie = builder.getMovie();
     }
 }
